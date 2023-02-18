1603eb3f8f309dac19cd51f5a0c942601a9f2ac8
aarian <ahmed.arian@fera.co.uk>
2023-02-18 14:55:16 +0000

    initial-commit

---
+++ android/app/src/main/java/com/vole/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vole.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vole.app',
+  appName: 'Vole Chauffeur',
+  webDir: 'dist/vole',
+  bundledWebRuntime: false
+};
+
+export default config;
